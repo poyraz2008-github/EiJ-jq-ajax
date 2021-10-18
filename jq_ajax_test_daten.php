0ac995ddbe81490e7a865ff6fac3262f2fb636a9
poyraz2008-github <76093621+poyraz2008-github@users.noreply.github.com>
2021-10-18 14:36:33 +0300

    Add files via upload

---
+++ jq_ajax_test_daten.php
@@ -0,0 +1,5 @@
+﻿<?php
+   header("Content-type: text/html; charset=utf-8");
+   echo "<b>Summe</b> der gesendeten Daten aus PHP-Datei: "
+      . (doubleval($_POST["zahl1"]) + doubleval($_POST["zahl2"]));
+?>
